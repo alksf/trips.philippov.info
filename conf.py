b6c888dd3c20cb8a4aa08faf577a169c41aebbcf
Aleksey Philippov <alks.philippov@gmail.com>
2020-03-12 11:16:11 +0300

    INDEX_DISPLAY_POST_COUNT = 50

--- conf.py
+++ conf.py
@@ -610,7 +610,7 @@ SHOW_SOURCELINK = False
 
 # Modify the number of Post per Index Page
 # Defaults to 10
-# INDEX_DISPLAY_POST_COUNT = 10
+INDEX_DISPLAY_POST_COUNT = 50
 
 # By default, Nikola generates RSS files for the website and for tags, and
 # links to it.  Set this to False to disable everything RSS-related.
